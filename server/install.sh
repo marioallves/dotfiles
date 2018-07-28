980201300c2c59a3d9feee93861f562f3837fb5a
Mário Alves <marioallves@gmail.com>
2018-07-28 07:42:34 +0100

    migrate changes conky

--- server/install.sh
+++ server/install.sh
@@ -2,3 +2,6 @@
 
 rm -f $HOME/.i3/config
 ln -s $HOME/.dotfiles/server/i3/config $HOME/.i3/config
+rm -f $HOME/.Xdefaults
+ln -s $HOME/.dotfiles/server/Xdefaults $HOME/.Xdefaults
+ln -s $HOME/.dotfiles/server/scripts /usr/local/bin
